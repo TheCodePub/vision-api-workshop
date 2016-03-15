75f024d1ba810507b0427fbbf2c69fc1aa43ca92
Thibaut Remy <thibaut.remy@fyndiq.com>
2016-03-15 19:08:28 +0100

    Add example LOGO drawing using CSS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+var resultDiv = $('.result');
+
 /*
  *
  * Start doing things with the Vision API response here!
  *
  */
 function handleVisionResult(result) {
-  
+
+  // Example : draw the IKEA logo
+  resultDiv.html(
+    '<div class="logo-ikea">' +
+      '<span>Ikea</span>' +
+    '</div>'
+  )
 }
